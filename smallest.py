534e0c98962efec7ab86fd7958ef498b77b08f39
Ganesh Koli <ganeshkoli0149@gmail.com>
2025-07-21 22:34:17 +0530

    gk

---
+++ smallest.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the number a:"))
+b=int(input("Enter the number b:"))
+c=int(input("Enter the number c:"))
+
+small=min(a,b,c)
+
+print("The smallest number is :",small)
+          
