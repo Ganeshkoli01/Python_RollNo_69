828b5ce300726a84cac2f215ad60d0389dc9d199
Ganesh Koli <ganeshkoli0149@gmail.com>
2025-07-31 15:23:37 +0530

    list

---
+++ listpractice.py
@@ -0,0 +1,29 @@
+l=[1,2,3,4,"gk",5.9]
+print(l)
+
+# l.append(3387)
+# print(l)
+
+# l.insert(2,"ganesh")
+# print(l)
+
+# l.remove("gk")
+# print(l)
+
+
+
+# numbers = list((3,2,5,4,1,6,7,8,9))
+# print(numbers)
+
+# numbers.sort()
+# print(numbers)
+
+# numbers.reverse()
+# print(numbers)
+
+print(l[0])
+print(l[1])
+print(l[2])
+print(l[3])
+print(l[4])
+print(l[5])
